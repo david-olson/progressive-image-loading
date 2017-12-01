e3659d8b6e0661d8ef6e315a0394ee05b7eb37ca
Dave Olson <dave@blueion.com>
2017-12-01 12:46:01 -0500

    Add error catch

--- progressive-image-loading/src/progressive-image-loading.js
+++ progressive-image-loading/src/progressive-image-loading.js
@@ -1,30 +1,33 @@
 var placeholders = document.getElementsByClassName('pil-placeholder');
 window.onload = function() {
-	console.log(placeholders);
-	for (i = 0; i < placeholders.length; i++) {
-		var small = placeholders[i].getElementsByTagName('img')[0];
-		var ratio = (placeholders[i].dataset.ratioHeight / placeholders[i].dataset.ratioWidth) * 100;
-		var large = placeholders[i].dataset.large;
-		// Update padding on .pil-height
-		if (placeholders[i].getElementsByClassName('pil-height')[0] !== null && placeholders[i].getElementsByClassName('pil-height')[0] !== false && placeholders[i].getElementsByClassName('pil-height')[0] !== undefined) {
-			placeholders[i].getElementsByClassName('pil-height')[0].style.paddingBottom = ratio + '%';
-		} else {
-			var pilHeight = document.createElement('div');
-			pilHeight.style.paddingBottom = ratio + '%';
-			placeholders[i].appendChild(pilHeight);
-		}
 
-		var img = new Image();
-		img.src = small.src;
-		img.onload = function() {
-			small.classList.add('loaded');
-		}
+	if (placeholders !== null && placeholders !== undefined && placeholders !== false) {
+		for (i = 0; i < placeholders.length; i++) {
+			var small = placeholders[i].getElementsByTagName('img')[0];
+			var ratio = (placeholders[i].dataset.ratioHeight / placeholders[i].dataset.ratioWidth) * 100;
+			var large = placeholders[i].dataset.large;
+			// Update padding on .pil-height
+			if (placeholders[i].getElementsByClassName('pil-height')[0] !== null && placeholders[i].getElementsByClassName('pil-height')[0] !== false && placeholders[i].getElementsByClassName('pil-height')[0] !== undefined) {
+				placeholders[i].getElementsByClassName('pil-height')[0].style.paddingBottom = ratio + '%';
+			} else {
+				var pilHeight = document.createElement('div');
+				pilHeight.style.paddingBottom = ratio + '%';
+				placeholders[i].appendChild(pilHeight);
+			}
 
-		var imgLarge = new Image();
-		imgLarge.src = large;
-		imgLarge.onload = function() {
-			this.classList.add('loaded');
+			var img = new Image();
+			img.src = small.src;
+			img.onload = function() {
+				small.classList.add('loaded');
+			}
+
+			var imgLarge = new Image();
+			imgLarge.src = large;
+			imgLarge.onload = function() {
+				this.classList.add('loaded');
+			}
+			placeholders[i].appendChild(imgLarge);
 		}
-		placeholders[i].appendChild(imgLarge);
 	}
+	
 }
